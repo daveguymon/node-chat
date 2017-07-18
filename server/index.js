7ab1883b2bfdc0b76356d99a525ed1464d09698d
Dave Guymon <davidwguymon@gmail.com>
2017-07-18 15:49:50 -0600

    Completed project

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const messages = require(__dirname+'/controllers/messages_controller');
+
+const app = express();
+app.use( bodyParser.json() );
+app.use(express.static(__dirname+'/../public/build'));
+
+const messagesBaseUrl = "/api/messages";
+app.post( messagesBaseUrl, messages.create );
+app.get( messagesBaseUrl, messages.read );
+app.put( `{messagesBaseUrl}:id`, messages.update );
+app.delete( `{messagesBaseUrl}:id`, messages.delete );
+
+const port = 3000;
+app.listen(port, ()=> { console.log(`Server listening on port ${port}`); } );
